789ce07c450e444636206e41069c24b67b561763
ZihaoChen <zihao.chen-4@student.manchester.ac.uk>
2023-07-24 19:02:00 +0800

    finish the study of RNN

---
+++ chapter_5/basic_component.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 22 10:36:19 2023
+
+@author: robert
+"""
+
+import numpy as np
+import book_given_function
+
+class RNN:
+    def __init__(self, Wx, Wh, b):
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.cache = None
+        
+    def forward(self, x, h_prev):
+        Wx, Wh, b = self.params
+        t = np.dot(h_prev, Wh) + np.dot(x, Wx) + b
+        h_next = np.tanh(t)
+        
+        self.cache = (x, h_prev, h_next)
+        return h_next
+    
+    def backward(self, dh_next):
+        Wx, Wh, b = self.params
+        x, h_prev, h_next = self.cache
+        
+        dt = dh_next * (1 - h_next**2)  # deivation of the tanh(x)
+        db = np.sum(dt, axis=0)  # backward for the repeat node
+        
+        dWh = np.dot(h_prev.T, dt)
+        dh_prev = np.dot(dt, Wh.T)
+        
+        dWx = np.dot(x.T, dt)
+        dx = np.dot(dt, Wx.T)
+        
+        self.grads[0][...] = dWx
+        self.grads[1][...] = dWh
+        self.grads[2][...] = db
+        
+        return dx, dh_prev
+        
+        
+class TimeRNN:
+    def __init__(self, Wx, Wh, b, stateful=False):
+        # stateful: whether we store the hidden state of each layer
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.layers = None  # so many layers that we needed
+        # h:  the last hidden state of forward()
+        # dh: ready for backward()
+        self.h, self.dh = None, None
+        self.stateful = stateful
+        
+    def set_state(self, h):
+        self.h = h
+        
+    def reset_state(self):
+        self.h = None
+        
+    def forward(self, xs):
+        Wx, Wh, b = self.params
+        N, T, D = xs.shape
+        D, H = Wx.shape
+        self.layers = []
+        hs = np.empty((N, T, H), dtype='f')
+        
+        if not self.stateful or self.h is None:
+            self.h = np.zeros((N, H), dtype='f')
+            
+        for t in range(T):
+            layer = RNN(*self.params)
+            self.h = layer.forward(xs[:, t, :], self.h)
+            hs[:, t, :] = self.h
+            self.layers.append(layer)
+            
+        return hs
+        
+    def backward(self, dhs):
+        # get the arguments that we needed
+        Wx, Wh, b = self.params
+        N, T, H = dhs.shape
+        D, H = Wx.shape
+        
+        # ready for the output
+        dxs = np.empty((N, T, D), dtype='f')
+        dh = 0  # useless for now, helpful to seq2seq
+        grads = [0, 0, 0]  # Wx, Wh, b
+        
+        for t in reversed(range(T)):
+            layer = self.layers[t]  # reversed layers
+            
+            dx, dh = layer.backward(dhs[:, t, :] + dh)  # each sequence data by "t"
+            dxs[:, t, :] = dx  # update the output
+            
+            for i, grad in enumerate(layer.grads):
+                grads[i] += grad  # consider every word! not just one, accumulating them
+            
+        for i, grad in enumerate(layer.grads):
+            self.grads[i][...] = grad  # OK, update THREE gradient values
+            
+        self.dh = dh  # useless for now, helpful to seq2seq a non-zero value
+        
+        return dxs
+    
+    
+class SimpleRnnLM:
+    def __init__(self, vocab_size, wordvec_size, hidden_size):
+        V, D, H = vocab_size, wordvec_size, hidden_size
+        rn = np.random.randn
+        embed_W = (rn(V, D)/100).astype('f')
+        rnn_Wx = (rn(D, H)/np.sqrt(D)).astype('f')
+        rnn_Wh = (rn(H, H)/np.sqrt(H)).astype('f')
+        rnn_b = np.zeros(H).astype('f')
+        affine_W = (rn(H, V)/np.sqrt(H)).astype('f')
+        affine_b = np.zeros(V).astype('f')
+        
+        self.layers = [
+            book_given_function.TimeEmbedding(embed_W),
+            book_given_function.TimeRNN(rnn_Wx, rnn_Wh, rnn_b, stateful=True),
+            book_given_function.TimeAffine(affine_W, affine_b)
+            ]
+        self.loss_layer = book_given_function.TimeSoftmaxWithLoss()
+        self.rnn_layer = self.layers[1]
+        
+        self.params, self.grads = [], []
+        for layer in self.layers:
+            self.params += layer.params
+            self.params += layer.grads
+            
+    def forward(self, xs, ts):
+        for layer in self.layers:
+            xs = layer.forward(xs)
+        loss = self.loss_layer.forward(xs, ts)
+        return loss
+    
+    def backward(self, dout=1):
+        dout = self.loss_layer.backward(dout)
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+        return dout
+    
+    def reset_state(self):
+        self.rnn_layer.reset_state()
+        
+        
+        
---
+++ chapter_5/book_given_function.py
@@ -0,0 +1,167 @@
+import numpy as np
+
+
+def softmax(x):
+    if x.ndim == 2:
+        x = x - x.max(axis=1, keepdims=True)
+        x = np.exp(x)
+        x /= x.sum(axis=1, keepdims=True)
+    elif x.ndim == 1:
+        x = x - np.max(x)
+        x = np.exp(x) / np.sum(np.exp(x))
+
+    return x
+
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+        if GPU:
+            np.scatter_add(dW, self.idx, dout)
+        else:
+            np.add.at(dW, self.idx, dout)
+        return None
+    
+    
+class TimeEmbedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.layers = None
+        self.W = W
+
+    def forward(self, xs):
+        N, T = xs.shape
+        V, D = self.W.shape
+
+        out = np.empty((N, T, D), dtype='f')
+        self.layers = []
+
+        for t in range(T):
+            layer = Embedding(self.W)
+            out[:, t, :] = layer.forward(xs[:, t])
+            self.layers.append(layer)
+
+        return out
+
+    def backward(self, dout):
+        N, T, D = dout.shape
+
+        grad = 0
+        for t in range(T):
+            layer = self.layers[t]
+            layer.backward(dout[:, t, :])
+            grad += layer.grads[0]
+
+        self.grads[0][...] = grad
+        return None
+
+
+class TimeAffine:
+    def __init__(self, W, b):
+        self.params = [W, b]
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
+
+    def forward(self, x):
+        N, T, D = x.shape
+        W, b = self.params
+
+        rx = x.reshape(N*T, -1)
+        out = np.dot(rx, W) + b
+        self.x = x
+        return out.reshape(N, T, -1)
+
+    def backward(self, dout):
+        x = self.x
+        N, T, D = x.shape
+        W, b = self.params
+
+        dout = dout.reshape(N*T, -1)
+        rx = x.reshape(N*T, -1)
+
+        db = np.sum(dout, axis=0)
+        dW = np.dot(rx.T, dout)
+        dx = np.dot(dout, W.T)
+        dx = dx.reshape(*x.shape)
+
+        self.grads[0][...] = dW
+        self.grads[1][...] = db
+
+        return dx
+
+
+class TimeSoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.cache = None
+        self.ignore_label = -1
+
+    def forward(self, xs, ts):
+        N, T, V = xs.shape
+
+        if ts.ndim == 3:  
+            ts = ts.argmax(axis=2)
+
+        mask = (ts != self.ignore_label)
+
+ \
+        xs = xs.reshape(N * T, V)
+        ts = ts.reshape(N * T)
+        mask = mask.reshape(N * T)
+
+        ys = softmax(xs)
+        ls = np.log(ys[np.arange(N * T), ts])
+        ls *= mask \
+        loss = -np.sum(ls)
+        loss /= mask.sum()
+
+        self.cache = (ts, ys, mask, (N, T, V))
+        return loss
+
+    def backward(self, dout=1):
+        ts, ys, mask, (N, T, V) = self.cache
+
+        dx = ys
+        dx[np.arange(N * T), ts] -= 1
+        dx *= dout
+        dx /= mask.sum()
+        dx *= mask[:, np.newaxis]  #
+
+        dx = dx.reshape((N, T, V))
+
+        return dx
+
+
+class TimeDropout:
+    def __init__(self, dropout_ratio=0.5):
+        self.params, self.grads = [], []
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+        self.train_flg = True
+
+    def forward(self, xs):
+        if self.train_flg:
+            flg = np.random.rand(*xs.shape) > self.dropout_ratio
+            scale = 1 / (1.0 - self.dropout_ratio)
+            self.mask = flg.astype(np.float32) * scale
+
+            return xs * self.mask
+        else:
+            return xs
+
+    def backward(self, dout):
+        return dout * self.mask
+
